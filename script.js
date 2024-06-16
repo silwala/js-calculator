da2726c9c20c026277626130fdcfe5678787f018
Aditya Silwal <silwala@sheridancollege.ca>
2024-06-16 00:32:40 -0400

    add dynamic height to the calculator by checking screen height with javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let calculator = document.querySelector('.calculator-container');
+let height = screen.height;
+let calculatorHeight = height * 0.7;
+console.log(calculator);
+console.log(height);
+calculator.style.height = `${calculatorHeight}px`
