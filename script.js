87dd26f58f142bd1e58331b3660438c16dfaeacf
Aditya Silwal <silwala@sheridancollege.ca>
2024-06-16 00:37:16 -0400

    cleanup the code

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 let calculator = document.querySelector('.calculator-container');
 let height = screen.height;
 let calculatorHeight = height * 0.7;
-console.log(calculator);
-console.log(height);
-// calculator.style.height = `${calculatorHeight}px`
