e378ae4e903fac88483440f3ad332b8ef7e44ff3
Aditya Silwal <silwala@sheridancollege.ca>
2024-06-16 00:36:33 -0400

    remove the dynamic height calculation
    changes height of the calculator if the window is resized and page is reloaded

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let height = screen.height;
 let calculatorHeight = height * 0.7;
 console.log(calculator);
 console.log(height);
-calculator.style.height = `${calculatorHeight}px`
+// calculator.style.height = `${calculatorHeight}px`
